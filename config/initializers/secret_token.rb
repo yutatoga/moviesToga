f9606ef3636660bfe7a2763bab9c1666e018566a
yutatoga <yuta.toga@gmail.com>
2013-07-18 23:46:12 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoviesToga::Application.config.secret_key_base = '8347c1c2611c679b500e8dce28d6dbcd89cc8a3adb6a513ee015baba3934a6ad0d9bad98d14fac8dafdb2def3722b22cda5d23dd1feaed79d573a3dad5d76350'
